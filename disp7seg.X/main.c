cbf4f7bab89f2dc89ccb653ff6fb46d2d23d28be
20185327 <20185327@DSN1271158622.cfp127aluno.local>
2021-02-25 16:52:07 -0300

    criação

--- disp7seg.X/main.c
+++ disp7seg.X/main.c
@@ -2,58 +2,12 @@
  * File:   main.c
  * Author: 20185327
  *
- * Created on 19 de Fevereiro de 2021, 15:14
+ * Created on 25 de Fevereiro de 2021, 16:51
  */
 
 
 #include <xc.h>
 
-void main(void) 
-{ int estado = 0;
-  int t;
- while(1)
- { 
-     switch(estado)
-             
-       case 0:  
-               estado  = 1;
-               break;
-       case 1:
-               semaforo_init();
-               estado = 2;
-               break;
-       case 2:
-               amarelo(0);
-               vermelho(0);
-               verde(1);
-               verdePed(0);
-               vermelhoPed(1);
-               if( botao() == 1 )
-                   estado = 3;
-               break;
-       case 3:
-              t = 3000;
-           estado = 4;
-                 
- 
- 
- 
- 
- 
- 
- 
- }    
-
-
-
-
-
-
-
-
-
-
-
-
-
+void main(void) {
+    return;
 }
